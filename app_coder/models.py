3b8312f811211c1bd0bd157e9b4701e50e62dde6
Juan Francisco Pineda <franciscopineda@habi.co>
2022-05-26 03:37:13 -0500

    [Feature] CRUD using Django views. Preparation class 22

--- app_coder/models.py
+++ app_coder/models.py
@@ -5,11 +5,17 @@ class Course(models.Model):
     name = models.CharField(max_length=40)
     code = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.name} course --'
+
 
 class Student(models.Model):
     name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+
+    def __str__(self):
+        return f'Nombre del Estudiante: {self.name} {self.last_name} -- e-mail: {self.email}'
 
 
 class Profesor(models.Model):
@@ -18,8 +24,15 @@ class Profesor(models.Model):
     email = models.EmailField()
     profession = models.CharField(max_length=40)
 
+    def __str__(self):
+        return f'Nombre del Profesor: {self.name} {self.last_name} -- e-mail: {self.email} -- profesión: {self.profession} --'
+
 
 class Homework(models.Model):
     name = models.CharField(max_length=40)
     due_date = models.DateField()
     is_delivered = models.BooleanField()
+
+    def __str__(self):
+        is_delivered = 'Si' if self.is_delivered else 'No'
+        return f'Nombre de la Entrega: {self.name} -- Fecha de entrega: {self.due_date} -- Entregado: {is_delivered}'
--- app_coder/urls.py
+++ app_coder/urls.py
@@ -2,15 +2,26 @@ from django.urls import path
 
 from app_coder import views
 
+app_name='app_coder'
 urlpatterns = [
     path('', views.index, name='Home'),
     path('profesors', views.profesors, name='Profesors'),
-    path('courses', views.courses, name='Courses'),
+    # path('courses', views.courses, name='course-list'),
     path('students', views.students, name='Students'),
     path('homeworks', views.homeworks, name='Homeworks'),
     path('formHTML', views.form_hmtl),
     path('course-django-forms', views.course_forms_django, name='CourseDjangoForms'),
     path('profesor-django-forms', views.profesor_forms_django, name='ProfesorDjangoForms'),
+    path('profesor/<int:pk>/update', views.update_profesor, name='UpdateProfesor'),
+    path('profesor/<int:pk>/delete', views.delete_profesor, name='DeleteProfesor'),
     path('homework-django-forms', views.homework_forms_django, name='HomeworkDjangoForms'),
     path('search', views.search, name='Search'),
+
+
+    # Dajngo documentation -->  https://docs.djangoproject.com/en/4.0/topics/class-based-views/generic-editing/
+    path('courses', views.CourseListView.as_view(), name='course-list'),
+    path('course/add/', views.CourseCreateView.as_view(), name='course-add'),
+    path('course/<int:pk>/detail', views.CourseDetailView.as_view(), name='course-detail'),
+    path('course/<int:pk>/update', views.CourseUpdateView.as_view(), name='course-update'),
+    path('course/<int:pk>/delete', views.CourseDeleteView.as_view(), name='course-delete'),
 ]
