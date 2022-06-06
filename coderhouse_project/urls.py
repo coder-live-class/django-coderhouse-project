b4a860032257dc501db8b9384c37284ea92a74ba
Juan Francisco Pineda <franciscopineda@habi.co>
2022-06-06 01:37:00 -0500

    [Feature] Refactor the project; split apps and improve configurations

--- coderhouse_project/urls.py
+++ coderhouse_project/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -28,4 +27,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+     from django.conf.urls.static import static
+     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
