909f2887fc9ae1174f9481dd40c2d23e0b76d02c
Juan Francisco Pineda <franciscopineda@habi.co>
2022-06-06 03:26:12 -0500

    Feature: Heroku configuration

--- coderhouse_project/settings.py
+++ coderhouse_project/settings.py
@@ -15,8 +15,6 @@ import environ
 import os
 from pathlib import Path
 
-django_heroku.settings(locals())
-
 env = environ.Env()
 environ.Env.read_env()
 
@@ -56,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,7 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -145,9 +149,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Heroku configs
+django_heroku.settings(locals())
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
