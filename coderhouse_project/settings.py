3fbdd3349c536d1fdbd783594d6413a1f74efdf7
Juan Francisco Pineda <franciscopineda@habi.co>
2022-06-06 02:21:56 -0500

    Automatic configuration with django-heroku

--- coderhouse_project/settings.py
+++ coderhouse_project/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 import os
 import environ
 from pathlib import Path
+
+django_heroku.settings(locals())
 
 env = environ.Env()
 environ.Env.read_env()
